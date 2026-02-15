f5f378e9ec125f12ec5c7a4fa8d36fd19573b581
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2026-02-15 13:29:31 +0000

    feat: update shard experiment script

--- run_experiment_shard.sh
+++ run_experiment_shard.sh
@@ -3,14 +3,33 @@ set -exo pipefail
 
 NUM_GPUS=${1:-}
 if [[ -z "$NUM_GPUS" ]]; then
-  echo "Usage: ./run_experiment.sh <num_gpus>"
+  echo "Usage: ./run_experiment_shard.sh <num_gpus>"
   exit 1
 fi
 
-echo "---Running with Sharding Choice: simple_ddp + zero 1---"
-torchrun --standalone --nproc_per_node=$NUM_GPUS main_shard.py --shard-choice zero1
+echo "---Running with Sharding Choice: baseline---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice baseline
 sleep 5
 
+echo "---Running with Sharding Choice: zero1---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice zero1
 sleep 5
-echo "---Running with Sharding Choice: pytorch_ddp + pytorch_zero1---"
-torchrun --standalone --nproc_per_node=$NUM_GPUS main_shard.py --shard-choice pytorch_zero1
+
+echo "---Running with Sharding Choice: zero2---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice zero2
+sleep 5
+
+echo "---Running with Sharding Choice: zero3---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice zero3
+sleep 5
+
+echo "---Running with Sharding Choice: pytorch_zero1---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice pytorch_zero1
+sleep 5
+
+echo "---Running with Sharding Choice: pytorch_zero2---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice pytorch_zero2
+sleep 5
+
+echo "---Running with Sharding Choice: pytorch_zero3---"
+torchrun --standalone --nproc_per_node=$NUM_GPUS main_ddp_shard.py --shard-choice pytorch_zero3
